8042f473a3b94b043ea7500d3880022f0304680d
Hayden Huy <hayden.huyp@gmail.com>
2023-10-11 17:55:14 -0300

    Add Open Youtube Video function

--- app/src/main/java/com/techcare/techcare/MainActivity.java
+++ app/src/main/java/com/techcare/techcare/MainActivity.java
@@ -2,7 +2,9 @@ package com.techcare.techcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // when button is clicked, starts callActivity
 
-        findViewById(R.id.call_button).setOnClickListener(v -> {
+        findViewById(R.id.btn_call).setOnClickListener(v -> {
             Intent intent = new Intent(this, CallActivity.class);
             startActivity(intent);
         });
 
+        findViewById(R.id.btn_youtube).setOnClickListener(v -> {
+            String id = "QVsCHTnt9mo";
+            Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + id));
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://www.youtube.com/watch?v=" + id));
+            try {
+                startActivity(appIntent);
+            } catch (ActivityNotFoundException ex) {
+                startActivity(webIntent);
+            }
+        });
     }
 }
