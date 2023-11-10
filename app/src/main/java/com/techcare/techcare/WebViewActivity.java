a100467e73044da3bc48f0441f2e14c224007dbd
Hayden Huy <hayden.huyp@gmail.com>
2023-11-10 12:08:28 -0400

    Add Back Btn on Webview screen

--- app/src/main/java/com/techcare/techcare/WebViewActivity.java
+++ app/src/main/java/com/techcare/techcare/WebViewActivity.java
@@ -1,6 +1,8 @@
 package com.techcare.techcare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,14 @@ public class WebViewActivity extends AppCompatActivity {
         // Đặt giao diện của bạn để hiển thị WebView
         setContentView(R.layout.activity_webview);
 
+        // go back to main activity when click on back button
+        findViewById(R.id.btnBackWebView).setOnClickListener(v -> {
+            // Log
+            Log.w("WebViewActivity", "Back button clicked");
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
         webView = findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
@@ -22,15 +32,6 @@ public class WebViewActivity extends AppCompatActivity {
         // Nếu bạn có một ID video YouTube cụ thể để tải, bạn có thể truyền nó như một extra trong intent
         String videoId = getIntent().getStringExtra("VIDEO_ID");
         webView.loadUrl("https://www.youtube.com/watch?v=" + videoId);
-    }
 
-    // Xử lý nút quay lại cho WebView
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
     }
 }
