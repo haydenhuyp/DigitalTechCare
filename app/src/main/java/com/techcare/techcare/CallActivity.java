1d1e7212febcf91bf13d701ec0045598abf5b6ca
Hayden Huy <hayden.huyp@gmail.com>
2023-11-16 21:44:36 -0400

    Update Zegocloud API Keys

--- app/src/main/java/com/techcare/techcare/CallActivity.java
+++ app/src/main/java/com/techcare/techcare/CallActivity.java
@@ -23,8 +23,8 @@ public class CallActivity extends AppCompatActivity {
     }
 
     public void addCallFragment() {
-        long appID = 434501726;
-        String appSign = "ea3bef30fb6d3b29b11f2c6840e8923059bab0b8b6615e77b489ed320ddfb6f5";
+        long appID = 2123839746;
+        String appSign = "19d9efe898b88872ae29dc185adae5520ddb87bd29b717d08db5913d17ea1968";
         // Generate a random username
         String userID = generateUserID();
         String userName = userID + "_Name";
